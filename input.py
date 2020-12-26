5462d70b458aa9eea3ba4dc9295bea07a8aa01d3
jacob <ajdkfl6445@gmail.com>
2020-12-26 23:45:24 +0900

    Study input

---
+++ input.py
@@ -0,0 +1,14 @@
+# msg = input('입력해:')
+# print(msg)
+#
+# prompt = "안녕하세요"
+# prompt += "\n이름이 뭐니: "
+#
+# msg = input(prompt)
+# print(f"ㄴㅓ의 이름은... {msg}ㅇㅣ구나")
+
+age = input('몇 살이니?')
+if 30 <= int(age):
+    print('많이 묵었땅..')
+else:
+    print('좋을때야')
